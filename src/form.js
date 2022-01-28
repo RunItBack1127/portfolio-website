ba6c67823c17f280211a0f0887d0e8be61458337
wpgreene <wpgreene@ncsu.edu>
2022-01-27 19:32:47 -0500

    Start of dynamic form content handling

--- src/form.js
+++ src/form.js
@@ -3,6 +3,12 @@ const FORMSPREE_POST_URL = "https://formspree.io/f/xwkypeke";
 const FORM_SUCCESS_RESPONSE = 200;
 const EMAIL_PROCESSING_ERR_RESPONSE = 422;
 
+const clearBtn = document.querySelector("#site-contact-form #send-clear-container #clear-btn");
+clearBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    clearFormInputs();
+});
+
 const formInputs = [...document.querySelectorAll(".form-field-container input"),
     document.querySelector("#site-contact-form #message-container textarea")];
 for(const formInput of formInputs) {
@@ -13,6 +19,31 @@ for(const formInput of formInputs) {
         else {
             formInput.classList.remove("has-content");
         }
+
+        // IF there is at least some text in one of
+        // the fields, enable the clear button -
+        // otherwise, disable both the clear and the
+        // send buttons
+        const textInFields = formInputs.some((formInput) => {
+            return formInput.value !== "";
+        });
+        if(textInFields) {
+            clearBtn.classList.remove("option-disable");
+        }
+        else {
+            clearBtn.classList.add("option-disable");
+            sendBtn.classList.add("option-disable");
+        }
+
+        const allFormsFilledOut = formInputs.every((formInput) => {
+            return formInput.value !== "";
+        });
+        if(allFormsFilledOut) {
+            sendBtn.classList.remove("option-disable");
+        }
+        else {
+            sendBtn.classList.add("option-disable");
+        }
     });
 }
 
@@ -21,13 +52,8 @@ function clearFormInputs() {
         formInput.value = "";
         formInput.classList.remove("has-content");
     }
+    clearBtn.classList.add("option-disable");
 }
-
-const clearBtn = document.querySelector("#site-contact-form #send-clear-container #clear-btn");
-clearBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    clearFormInputs();
-});
 
 function RETRIEVE_FORM_DATA() {
     const formData = {
